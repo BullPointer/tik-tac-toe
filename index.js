64e62197402de6b5e60116c05c02fc53955020a5
White Bull <blueded88@gmail.com>
2023-04-06 05:31:31 -0400

    Commiting game

--- index.js
+++ index.js
@@ -1,34 +1,48 @@
 let gameboard = ['', '', '', '', '', '', '', '', ''];
+const winArray = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
 const Gameboard = (name, gameArray) => {
     const getName = () => name;
     const gameBoard = () => gameArray;
+    let currentPlayer = name;
 
     const makeContainerElem = () => {
         const container = document.createElement('div');
         container.classList.add('container');
         return container;
     }
-    const makeBoxElem = (index) => {
+    const makeBoxElem = (index, enemy) => {
         const box = document.createElement('div');
         box.classList.add('box');
+
         box.addEventListener('click', () => {
-            
-        })
+            currentPlayer = currentPlayer === name? enemy.getName(): name;
+            box.textContent = currentPlayer;
+            gameArray[index] = currentPlayer;
+        }, {once: true});
         return box;
     }
-    const resetGame = () => {
+    const resetGame = (enemy) => {
         gameContainerElem = makeContainerElem();
         gameBoxElem = makeBoxElem();
         for (let index = 0; index < 9; index++) {
-            gameContainerElem.appendChild(makeBoxElem(index));
+            gameContainerElem.appendChild(makeBoxElem(index, enemy));
         }
         document.body.appendChild(gameContainerElem);
     }
-    return {resetGame}
+    return {resetGame, getName, gameBoard}
 }
 
-const player = Gameboard('X', gameboard);
-const ai = Gameboard('O', gameboard);
+const player = Gameboard('O', gameboard, winArray);
+const ai = Gameboard('X', gameboard, winArray);
 
-player.resetGame();
+player.resetGame(ai);
 
